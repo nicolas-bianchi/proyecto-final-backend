e51f4d49d19714790cd40c98e39345a6892a9600
Felipe Ruete <feruete@gmail.com>
2022-12-02 17:06:07 -0300

    agregué un intento de formidable y fs que quedó comentado

--- controllers/estates.js
+++ controllers/estates.js
@@ -1,4 +1,6 @@
+const formidable = require("formidable");
 const knex = require("../config/knexFile");
+const fs = require("fs");
 
 exports.consultAllEstates = (req, res) => {
   knex("direcciones")
@@ -60,3 +62,84 @@ exports.newPublication = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+// PRUEBA FORMIDABLE Y FS
+// exports.newPublication = async (req, res) => {
+//   let form = new formidable.IncomingForm();
+//   form.keepExtensions = true;
+//   try {
+//     form.parse(req, (err, fields, files) => {
+//       if (err) {
+//         return res.status(400).json({
+//           error: "No se pudo cargar la imagen",
+//         });
+//       }
+//       const {
+//         precio,
+//         dormitorios,
+//         tipoInmueble,
+//         operacion,
+//         gastosComunes,
+//         metrosCuadrados,
+//         departamento,
+//         ciudad,
+//         barrio,
+//         calle,
+//       } = fields;
+//       if (!precio || !precio.length) {
+//         return res.status(400).json({ error: "El precio es obligatorio" });
+//       }
+//       try {
+//         const newDirecciones = knex("direcciones")
+//           .returning("direccionId")
+//           .insert({
+//             departamento: departamento,
+//             ciudad: ciudad,
+//             barrio: barrio,
+//             calle: calle,
+//           });
+//         const newInmuebles = knex("inmuebles").insert({
+//           precio: precio,
+//           dormitorios: dormitorios,
+//           tipoInmueble: tipoInmueble,
+//           operacion: operacion,
+//           gastosComunes: gastosComunes,
+//           metrosCuadrados: metrosCuadrados,
+//           direccionId: newDirecciones[0].direccionId,
+//         });
+//         res.json({ message: "Se ha ingresado el inmueble con éxito" });
+//       } catch (error) {
+//         res.status(400).json({ error: error.message });
+//       }
+
+//       let fileData;
+//       let fileType;
+//       if (files.file) {
+//         if (files.file.size > 1000000) {
+//           return res.status(400).json({
+//             error: "Tamaño maximo de la imagen 1MB",
+//           });
+//         }
+//         fileData = fs.readFileSync(files.file.filepath);
+//         fileType = files.file.mimetype;
+//         knex("inmuebles")
+//           .where("direccionId", newDirecciones[0].direccionId)
+//           .update({
+//             filedata: fileData,
+//             filetype: fileType,
+//           })
+//           .then(() => {
+//             res.json({ success: true });
+//             return;
+//           })
+//           .catch((error) => {
+//             res.status(400).json({ error: error.message });
+//             return;
+//           });
+//       }
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).json({ error: e });
+//   }
+// };
